db02d4cb1464a4dbee1d4a44316702e301e202bd
Rob <rob@websanova.com>
2026-06-07 19:01:40 -0400

    Setup spatie laravel backups.

--- routes/console.php
+++ routes/console.php
@@ -2,5 +2,7 @@
 
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('sanctum:prune-expired --hours=1')->daily();
-Schedule::command('users:prune-deleted')->daily();
+Schedule::command('sanctum:prune-expired --hours=1')->dailyAt('02:00')->timezone('America/New_York');
+Schedule::command('users:prune-deleted')->dailyAt('02:05')->timezone('America/New_York');
+Schedule::command('backup:run --only-db')->dailyAt('02:10')->timezone('America/New_York');
+Schedule::command('backup:clean')->dailyAt('02:15')->timezone('America/New_York');
