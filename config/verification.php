3e48484019db90292555c84fa26fd5fac4bff114
websanova <rob@websanova.com>
2026-07-05 19:10:42 -0400

    Disable verification.

--- config/verification.php
+++ config/verification.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'mode' => VerificationMode::from(env('VERIFICATION_MODE', 'required')),
+    'mode' => VerificationMode::from(env('VERIFICATION_MODE', 'disabled')),
 
     /*
     |--------------------------------------------------------------------------
