240f1d286d05ff4f4c0ddd0cfd880c7134141853
websanova <rob@websanova.com>
2026-09-07 11:56:10 -0400

    Remove the unnecessary subscribed authorize checks in subscription cancel and resume.

--- app/Http/Requests/App/SubscriptionResume/StoreRequest.php
+++ app/Http/Requests/App/SubscriptionResume/StoreRequest.php
@@ -7,14 +7,6 @@ use Illuminate\Foundation\Http\FormRequest;
 class StoreRequest extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return $this->user()->subscribed();
-    }
-
-    /**
      * Get the validation rules that apply to the request.
      */
     public function rules(): array
