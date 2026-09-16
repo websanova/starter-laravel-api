25a187b76ded258bbfd5f482d1169886749813dc
websanova <rob@websanova.com>
2026-09-15 20:42:23 -0400

    Add bookmark_max_tags to settings.

--- app/Http/Controllers/App/SettingController.php
+++ app/Http/Controllers/App/SettingController.php
@@ -16,6 +16,7 @@ class SettingController extends Controller
     {
         return response()->json([
             'data' => [
+                'bookmark_max_tags' => config('bookmark.max_tags'),
                 'subscription_automatic_tax' => config('subscription.automatic_tax'),
                 'subscription_card_upfront' => config('subscription.require_card_upfront'),
                 'subscription_mode' => config('subscription.mode')->value,
